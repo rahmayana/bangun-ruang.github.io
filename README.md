ad869d27ee1bae64da94dee8c7eee47d0ba0d909
rahmayana <yanaruslan701@gmail.com>
2024-05-06 13:49:00 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# bangun-ruang.github.io
---
+++ story_content/user.js
@@ -0,0 +1,18 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "5c42wuEF1Wb":
+        Script1();
+        break;
+  }
+}
+
+function Script1()
+{
+  var.audio = document.getElementById('bgSong');
+audio.src="musik.mp3";
+audio.load();
+audio.play();
+}
+
